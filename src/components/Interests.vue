4b45c0c6b0f5b48cde54e04330162ffb545d5fe5
Sam <samjmarshall1@live.com>
2019-08-10 21:46:18 +1200

    updated content again

--- src/components/Interests.vue
+++ src/components/Interests.vue
@@ -4,9 +4,9 @@
       <h2 class="mb-5">Interests</h2>
       <p>Apart from being a Performance tester, I love to play Basketball, Football, Go to the gym and run. I also love movies and music.  </p>
       <p class="mb-5">I like to stay up to date with current tech. I love reading Twitter, Reddit and Blogs to find new interesting things going on. I have a real love for sports analytics with my favourite sports being Basketball and Football </p>
+      <g-image src="~/image.png" width="500" fit="contain"/>
     </div>
-  <div class="w-100">
-  <g-image class="mb-0" src="~/image.png" width="400"/>
-</div>
-  </section>
+
+
+</section>
 </template>
