be4183dfcbb57ad2031ef20c1d22c370ba9e1c8a
Sam <samjmarshall1@live.com>
2019-08-11 12:31:29 +1200

    cropped photo

--- src/components/Interests.vue
+++ src/components/Interests.vue
@@ -4,7 +4,7 @@
       <h2 class="mb-5">Interests</h2>
       <p>Apart from being a performance tester, I am sports mad! I love to play basketball, football, tennis and volleyball. I go to the gym regularly and have also recently starting running. I completed my first half marathon last November and have another one in October this year. I also love movies and music from all eras and genres.  </p>
       <p class="mb-5">I enjoy keeping up to date on Twitter, Reddit, blogs and podcasts to keep abreast of new trends, global happenings and areas of interest to me. I have a real love for following sport, especially with the consent developments in sport data analytics and technologies. There is never a dull moment in the sports world.</p>
-      <g-image src="~/image.png" width="500" fit="contain"/>
+      <g-image src="~/half.png" width="500" fit="contain"/>
     </div>
 
 
